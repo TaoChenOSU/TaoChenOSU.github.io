1fc021548640ad1e9662b39f25b566c059b33f80
Tao Chen <taochen@microsoft.com>
2026-02-11 21:05:23 -0800

    Modern layout redesign: dark navy + gold theme
    - Hero section with two-column layout, bio, social links, dual CTAs
    - Featured section with 3-column card grid for blog posts, press, awards
    - Compact left-aligned timeline merging experience and education
    - Removed separate About, Education, Projects, and Skills sections
    - Dark navy (#0a192f) + gold (#ffc107) color scheme
    - Inter + Fira Code fonts, scroll-reveal animations
    - Fully responsive layout

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,6 @@
 /*!
-    Title: Dev Portfolio Template
-    Version: 1.2.1
-    Last Change: 08/27/2017
-    Author: Ryan Fitzgerald
-    Repo: https://github.com/RyanFitzgerald/devportfolio-template
-    Issues: https://github.com/RyanFitzgerald/devportfolio-template/issues
-
-    Description: This file contains all the scripts associated with the single-page
-    portfolio website.
+    Portfolio Scripts
+    Based on Dev Portfolio Template by Ryan Fitzgerald
 */
 
 (function($) {
@@ -17,19 +10,13 @@
 
     // Animate to section when nav is clicked
     $('header a').click(function(e) {
-
-        // Treat as normal link if no-scroll class
         if ($(this).hasClass('no-scroll')) return;
-
         e.preventDefault();
         var heading = $(this).attr('href');
         var scrollDistance = $(heading).offset().top;
-
         $('html, body').animate({
             scrollTop: scrollDistance + 'px'
         }, Math.abs(window.pageYOffset - $(heading).offset().top) / 3);
-
-        // Hide the menu once clicked if mobile
         if ($('header').hasClass('active')) {
             $('header, body').removeClass('active');
         }
@@ -37,43 +24,13 @@
 
     // Scroll to top
     $('#to-top').click(function() {
-        $('html, body').animate({
-            scrollTop: 0
-        }, 500);
+        $('html, body').animate({ scrollTop: 0 }, 500);
     });
 
-    // Scroll to first element
-    $('#lead-down span').click(function() {
-        var scrollDistance = $('#lead').next().offset().top;
-        $('html, body').animate({
-            scrollTop: scrollDistance + 'px'
-        }, 500);
-    });
-
-    // Create timeline
-    $('#experience-timeline').each(function() {
-
-        $this = $(this); // Store reference to this
-        $userContent = $this.children('div'); // user content
-
-        // Create each timeline block
-        $userContent.each(function() {
-            $(this).addClass('vtimeline-content').wrap('<div class="vtimeline-point"><div class="vtimeline-block"></div></div>');
-        });
-
-        // Add icons to each block
-        $this.find('.vtimeline-point').each(function() {
-            $(this).prepend('<div class="vtimeline-icon"><i class="fa fa-map-marker"></i></div>');
-        });
-
-        // Add dates to the timeline if exists
-        $this.find('.vtimeline-content').each(function() {
-            var date = $(this).data('date');
-            if (date) { // Prepend if exists
-                $(this).parent().prepend('<span class="vtimeline-date">'+date+'</span>');
-            }
-        });
-
+    // Scroll to first section from hero
+    $('#hero-scroll-down span').click(function() {
+        var scrollDistance = $('#hero').next().offset().top;
+        $('html, body').animate({ scrollTop: scrollDistance + 'px' }, 500);
     });
 
     // Open mobile menu
@@ -84,14 +41,6 @@
     // Close mobile menu
     $('#mobile-menu-close').click(function() {
         $('header, body').removeClass('active');
-    });
-
-    // Load additional projects
-    $('#view-more-projects').click(function(e){
-        e.preventDefault();
-        $(this).fadeOut(300, function() {
-            $('#more-projects').fadeIn(300);
-        });
     });
 
 })(jQuery);
